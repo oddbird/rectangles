5f322a9b15b545fab4250c992a651e9b7563492b
Jonny Gerig Meyer <jonny@oddbird.net>
2021-12-10 11:58:37 -0500

    Sample change to test GH integration.

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <svg id="chart" width="100%" height="500px"></svg>
+    <svg id="chart" width="100%" height="400px"></svg>
     <div class="example-scoped">This is styled from scoped styles.</div>
   </div>
 </template>
